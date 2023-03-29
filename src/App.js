b4e8467df1021ab1f4a6f6dbed50ae5e21d0ac18
sajedulislamsaju <sajedulislamsaju123@gmail.com>
2023-03-29 12:49:22 +0600

    this is the last commit :::

--- src/App.js
+++ src/App.js
@@ -1,33 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is the pappi songe .</h1>
-      <h2>this is the module 48</h2>
+    <div className="App">      
+      <LoadCountries></LoadCountries>
+      
+      
     </div>
   );
 }
 
+function LoadCountries () {
+  const [countries , setcountries] = useState([])
+
+  useEffect(() => {
+
+    fetch('https://restcountries.com/v3.1/all')
+    .then (res => res.json())
+    .then (data => setcountries(data))
+  },[])
+
+  return (
+    <div>
+      <h1>this is not working .</h1>
+      <h3>Available Conotries : {countries.length}</h3>
+      {
+        countries.map (country => <Country name= {country.name.common} population = {country.population} area={country.area} flags= {country.flags.png
+        }></Country>)
+      }
+    </div>
+  )
+}
+
+
+function Country (props){
+  return(
+    <div className='country'>
+      <img src={props.flags} alt="" />
+      <h1>Name : {props.name}</h1>
+      <h3>Population : {props.population}</h3>
+      <h5>Area : {props.area}</h5>
+    </div>
+  )
+}
+
 export default App;
-
-
-
-
-
-
-{/* <header className="App-header">
-  <img src={logo} className="App-logo" alt="logo" />
-  <p>
-    Edit <code>src/App.js</code> and save to reload.
-  </p>
-  <a
-    className="App-link"
-    href="https://reactjs.org"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    Learn React
-  </a>
-</header> */}
---
+++ src/Country/Person.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const person = () => {
+    return (
+        <div>
+            <h1>this is the header section in the main section</h1>
+        </div>
+    );
+};
+
+export default person;
