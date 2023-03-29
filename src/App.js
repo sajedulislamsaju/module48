f70cdba371abb2e505e3c047f335a0f45f315ef0
sajedulislamsaju <sajedulislamsaju123@gmail.com>
2023-03-29 13:12:06 +0600

    The Khati bangla is added in the header section

--- src/App.js
+++ src/App.js
@@ -2,49 +2,48 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 import { useEffect } from 'react';
+import Bangla from './Component/Banagram/Bangla';
 
 function App() {
   return (
     <div className="App">      
-      <LoadCountries></LoadCountries>
-      
-      
+        <Bangla></Bangla>
     </div>
   );
 }
 
-function LoadCountries () {
-  const [countries , setcountries] = useState([])
+// function LoadCountries () {
+//   const [countries , setcountries] = useState([])
 
-  useEffect(() => {
+//   useEffect(() => {
 
-    fetch('https://restcountries.com/v3.1/all')
-    .then (res => res.json())
-    .then (data => setcountries(data))
-  },[])
+//     fetch('https://restcountries.com/v3.1/all')
+//     .then (res => res.json())
+//     .then (data => setcountries(data))
+//   },[])
 
-  return (
-    <div>
-      <h1>this is not working .</h1>
-      <h3>Available Conotries : {countries.length}</h3>
-      {
-        countries.map (country => <Country name= {country.name.common} population = {country.population} area={country.area} flags= {country.flags.png
-        }></Country>)
-      }
-    </div>
-  )
-}
+//   return (
+//     <div>
+//       <h1>this is not working .</h1>
+//       <h3>Available Conotries : {countries.length}</h3>
+//       {
+//         countries.map (country => <Country name= {country.name.common} population = {country.population} area={country.area} flags= {country.flags.png
+//         }></Country>)
+//       }
+//     </div>
+//   )
+// }
 
 
-function Country (props){
-  return(
-    <div className='country'>
-      <img src={props.flags} alt="" />
-      <h1>Name : {props.name}</h1>
-      <h3>Population : {props.population}</h3>
-      <h5>Area : {props.area}</h5>
-    </div>
-  )
-}
+// function Country (props){
+//   return(
+//     <div className='country'>
+//       <img src={props.flags} alt="" />
+//       <h1>Name : {props.name}</h1>
+//       <h3>Population : {props.population}</h3>
+//       <h5>Area : {props.area}</h5>
+//     </div>
+//   )
+// }
 
 export default App;
---
+++ src/Component/Banagram/Bangla.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Bangla = () => {
+    return (
+        <div>
+            <h1>This is the header side of the banagram .</h1>
+        </div>
+    );
+};
+
+export default Bangla;
